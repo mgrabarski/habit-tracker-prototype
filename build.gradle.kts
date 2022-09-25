92efe405d08baf09d3cc7afe7f478048534ed5a1
Mateusz <mateusz.grabarski@gmail.com>
2022-09-25 13:28:56 +0200

    Init

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+buildscript {
+}
+
+plugins {
+    id("com.android.application") version "7.3.0" apply false
+    id("com.android.library") version "7.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.6.10" apply false
+    id("org.jlleitschuh.gradle.ktlint") version "11.0.0"
+    id("io.gitlab.arturbosch.detekt") version "1.21.0"
+}
+
+subprojects {
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    repositories {
+        mavenCentral()
+    }
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        debug.set(true)
+    }
+}
