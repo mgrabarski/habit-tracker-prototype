9d467983093fa2cbd5e1f9c87e5928c89f293a03
Mateusz <mateusz.grabarski@gmail.com>
2022-10-22 14:10:32 +0200

    Added MoveBackActionsTopBar previews

--- resources/src/main/java/com/mateuszgrabarski/habittracker/resources/ui/components/topbar/MoveBackActionsTopBar.kt
+++ resources/src/main/java/com/mateuszgrabarski/habittracker/resources/ui/components/topbar/MoveBackActionsTopBar.kt
@@ -1,5 +1,6 @@
 package com.mateuszgrabarski.habittracker.resources.ui.components.topbar
 
+import android.content.res.Configuration
 import androidx.annotation.StringRes
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
@@ -9,6 +10,7 @@ import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import com.mateuszgrabarski.habittracker.resources.R
 import com.mateuszgrabarski.habittracker.resources.ui.theme.topBarBackground
 import com.mateuszgrabarski.habittracker.resources.ui.theme.topBarContent
@@ -34,5 +36,23 @@ fun MoveBackActionsTopBar(
                 )
             }
         }
+    )
+}
+
+@Preview
+@Composable
+fun MoveBackActionsTopBarLight() {
+    MoveBackActionsTopBar(
+        title = R.string.add_habit_top_bar_title,
+        navigateBack = {}
+    )
+}
+
+@Preview(uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun MoveBackActionsTopBarDark() {
+    MoveBackActionsTopBar(
+        title = R.string.add_habit_top_bar_title,
+        navigateBack = {}
     )
 }
