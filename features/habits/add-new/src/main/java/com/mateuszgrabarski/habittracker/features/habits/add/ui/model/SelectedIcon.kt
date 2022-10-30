7a074d06c008ca5c8ceaa09bf05c53cec28d681b
Mateusz <mateusz.grabarski@gmail.com>
2022-10-30 20:04:03 +0100

    Added selected icon model

---
+++ features/habits/add-new/src/main/java/com/mateuszgrabarski/habittracker/features/habits/add/ui/model/SelectedIcon.kt
@@ -0,0 +1,8 @@
+package com.mateuszgrabarski.habittracker.features.habits.add.ui.model
+
+import com.mateuszgrabarski.habittracker.features.habits.add.ui.dialog.HabitIcon
+
+data class SelectedIcon(
+    val icon: HabitIcon,
+    val color: String
+)
