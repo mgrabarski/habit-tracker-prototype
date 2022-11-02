1aa070be93a239457bd40c1978d0a246e6b07839
Mateusz <mateusz.grabarski@gmail.com>
2022-11-02 20:20:18 +0100

    Added selected time model

--- features/habits/add-new/src/main/java/com/mateuszgrabarski/habittracker/features/habits/add/ui/model/SelectedTime.kt
+++ features/habits/add-new/src/main/java/com/mateuszgrabarski/habittracker/features/habits/add/ui/model/SelectedTime.kt
@@ -3,4 +3,7 @@ package com.mateuszgrabarski.habittracker.features.habits.add.ui.model
 data class SelectedTime(
     val hours: Int,
     val minutes: Int
-)
+) {
+    val displayValues: String
+        get() = "$hours:$minutes"
+}
