4b742fc5b661cfc21ff94f57541ec953ce754fd0
Mateusz <mateusz.grabarski@gmail.com>
2022-11-02 20:21:37 +0100

    Dependencies update

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,6 +11,6 @@ dependencies {
     implementation(gradleApi())
     implementation(localGroovy())
 
-    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+    implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
     implementation("com.android.tools.build:gradle:7.3.0")
 }
--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -31,7 +31,7 @@ object UnitTestDependencies {
     val mockkAgentJvm by lazy { "io.mockk:mockk-agent-jvm:${Versions.mockkAgentJvm}" }
     val turbine by lazy { "app.cash.turbine:turbine:${Versions.turbine}" }
     val androidxCoreTesting by lazy { "androidx.arch.core:core-testing:${Versions.androidxCoreTesting}" }
-    val koin by lazy { "io.insert-koin:koin-test-junit5:${Versions.koinAndroid}" }
+    val koin by lazy { "io.insert-koin:koin-test-junit5:${Versions.koinTest}" }
 }
 
 object IntegrationTestDependencies {
