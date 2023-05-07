b274d8fd8ecbb46a98c3f0c1237cde529f342885
Mateusz <mateusz.grabarski@gmail.com>
2023-05-07 13:20:19 +0200

    Gradle update

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,5 +12,5 @@ dependencies {
     implementation(localGroovy())
 
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
-    implementation("com.android.tools.build:gradle:7.3.0")
+    implementation("com.android.tools.build:gradle:8.0.1")
 }
