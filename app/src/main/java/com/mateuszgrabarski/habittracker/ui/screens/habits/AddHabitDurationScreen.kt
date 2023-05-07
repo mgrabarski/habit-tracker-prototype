fd3c5011b0f58d84a68feb8a1b7bec0dbe3700a1
Mateusz Grabarski <mateusz.grabarski@gmail.com>
2023-05-07 18:52:18 +0200

    Navigate back to habits list (#11)

--- app/src/main/java/com/mateuszgrabarski/habittracker/ui/screens/habits/AddHabitDurationScreen.kt
+++ app/src/main/java/com/mateuszgrabarski/habittracker/ui/screens/habits/AddHabitDurationScreen.kt
@@ -3,14 +3,24 @@ package com.mateuszgrabarski.habittracker.ui.screens.habits
 import androidx.compose.runtime.Composable
 import com.mateuszgrabarski.habittracker.business.data.models.habits.add.NewHabitBaseDefinition
 import com.mateuszgrabarski.habittracker.features.habits.add.ui.screen.AddNewHabitDurationScreen
+import com.mateuszgrabarski.habittracker.ui.screens.destinations.HabitsScreenDestination
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
 @Destination
 @Composable
 fun AddHabitDurationScreen(
-    habitBaseDefinition: NewHabitBaseDefinition
+    habitBaseDefinition: NewHabitBaseDefinition,
+    destinationsNavigator: DestinationsNavigator
 ) {
     AddNewHabitDurationScreen(
-        habitBaseDefinition = habitBaseDefinition
+        habitBaseDefinition = habitBaseDefinition,
+        moveBackToList = {
+            destinationsNavigator.navigate(HabitsScreenDestination) {
+                popUpTo(HabitsScreenDestination.route) {
+                    inclusive = true
+                }
+            }
+        }
     )
 }
